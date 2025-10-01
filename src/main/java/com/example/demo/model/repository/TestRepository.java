b7f58cdaf6811b4fe402e29296a2b23932dcde35
sang-10 <sanghyeonn4@gmail.com>
2025-10-01 13:48:52 +0900

    week5

--- src/main/java/com/example/demo/model/repository/TestRepository.java
+++ src/main/java/com/example/demo/model/repository/TestRepository.java
@@ -2,6 +2,7 @@
  import org.springframework.data.jpa.repository.JpaRepository; // JPA 필수 등록
 import org.springframework.stereotype.Repository; // 빈 등록
 import com.example.demo.model.domain.TestDB; // 도메인 연동
+
 @Repository // 리포지토리 등록
 public interface TestRepository extends JpaRepository<TestDB, Long> {
 // Find all TestDB entities by a name
