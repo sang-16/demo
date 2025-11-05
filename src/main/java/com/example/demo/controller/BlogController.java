10a555025c3e0b5ff75ac683ec33bfbb54a81e3c
sang-10 <sanghyeonn4@gmail.com>
2025-11-05 14:03:52 +0900

    WEEK10

--- src/main/java/com/example/demo/controller/BlogController.java
+++ src/main/java/com/example/demo/controller/BlogController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
  import com.example.demo.model.domain.Article;
- import com.example.demo.model.domain.TestDB;
+import com.example.demo.model.domain.Board;
+import com.example.demo.model.domain.TestDB;
  import java.util.List;
 import java.util.Optional;
 
@@ -24,24 +25,54 @@ public class BlogController {
  BlogService blogService; // DemoController 클래스 아래 객체 생성
 
     
- @GetMapping("/article_list") // 게시판 링크 지정
-public String article_list(Model model) {
-List<Article> list = blogService.findAll(); // 게시판 리스트
-model.addAttribute("articles", list); // 모델에 추가
-return "article_list"; // .HTML 연결
+ //@GetMapping("/article_list") // 게시판 링크 지정
+//public String article_list(Model model) {
+//List<Article> list = blogService.findAll(); // 게시판 리스트
+//model.addAttribute("articles", list); // 모델에 추가
+//return "article_list"; // .HTML 연결
+//}
+ @GetMapping("/board_list") // 새로운 게시판 링크 지정
+public String board_list(Model model) {
+ List<Board> list = blogService.findAll(); // 게시판 전체 리스트, 기존 Article에서 Board로 변경됨
+model.addAttribute("boards", list); // 모델에 추가
+return "board_list"; // .HTML 연결
 }
 
-@GetMapping("/article_edit/{id}") // 게시판 링크 지정
-public String article_edit(Model model, @PathVariable Long id) {
-Optional<Article> list = blogService.findById(id); // 선택한 게시판 글
+
+  @GetMapping("/board_view/{id}") // 게시판 링크 지정
+public String board_view(Model model, @PathVariable Long id) {
+ Optional<Board> list = blogService.findById(id); // 선택한 게시판 글
 if (list.isPresent()) {
-model.addAttribute("article", list.get()); // 존재하면 Article 객체를 모델에 추가
+ model.addAttribute("boards", list.get()); // 존재할 경우 실제 Board 객체를 모델에 추가
 } else {
+ // 처리할 로직 추가 (예: 오류 페이지로 리다이렉트, 예외 처리 등)
+ return "/error_page/article_error"; // 오류 처리 페이지로 연결
+}
+ return "board_view"; // .HTML 연결
+}
+
+
+
+
+
+
+
+
+
+
+
+
+//@GetMapping("/article_edit/{id}") // 게시판 링크 지정
+//public String article_edit(Model model, @PathVariable Long id) {
+//Optional<Article> list = blogService.findById(id); // 선택한 게시판 글
+//if (list.isPresent()) {
+//model.addAttribute("article", list.get()); // 존재하면 Article 객체를 모델에 추가
+//} else {
 // 처리할 로직 추가 (예: 오류 페이지로 리다이렉트, 예외 처리 등)
-return "/error_page/article_error"; // 오류 처리 페이지로 연결(이름 수정됨)
-}
-return "article_edit"; // .HTML 연결
-}
+//return "/error_page/article_error"; // 오류 처리 페이지로 연결(이름 수정됨)
+//}
+//return "article_edit"; // .HTML 연결
+//}
 
 @PutMapping("/api/article_edit/{id}")
 public String updateArticle(@PathVariable Long id, @ModelAttribute AddArticleRequest request) {
--- src/main/java/com/example/demo/model/service/BlogService.java
+++ src/main/java/com/example/demo/model/service/BlogService.java
@@ -5,7 +5,10 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.example.demo.model.domain.Article;
+import com.example.demo.model.domain.Board;
 import com.example.demo.model.repository.BlogRepository;
+import com.example.demo.model.repository.BoardRepository;
+
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -13,9 +16,14 @@ import lombok.RequiredArgsConstructor;
 public class BlogService {
 @Autowired // 객체 주입 자동화, 생성자 1개면 생략 가능
 private final BlogRepository blogRepository; // 리포지토리 선언
+private final BoardRepository blogRepository2; // 리포지토리 선언
 
-public List<Article> findAll() { // 게시판 전체 목록 조회
-return blogRepository.findAll();
+//public List<Article> findAll() { // 게시판 전체 목록 조회
+//return blogRepository.findAll();
+//}
+
+public List<Board> findAll() { // 게시판 전체 목록 조회
+return blogRepository2.findAll();
 }
 
 public Article save(AddArticleRequest request){
@@ -28,9 +36,14 @@ public Article save(AddArticleRequest request){
 return blogRepository.save(request.toEntity());
 }
 
-public Optional<Article> findById(Long id) { // 게시판 특정 글 조회
-return blogRepository.findById(id);
+//public Optional<Article> findById(Long id) { // 게시판 특정 글 조회
+//return blogRepository.findById(id);
+//}
+
+public Optional<Board> findById(Long id) { // 게시판 특정 글 조회
+return blogRepository2.findById(id);
 }
+
 public void update(Long id, AddArticleRequest request) {
 Optional<Article> optionalArticle = blogRepository.findById(id); // 단일 글 조회
 optionalArticle.ifPresent(article -> { // 값이 있으면
