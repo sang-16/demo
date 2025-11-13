def71bd929d986fd13f99aafd73993d353df0667
sang-10 <sanghyeonn4@gmail.com>
2025-11-13 14:39:34 +0900

    week11

--- src/main/java/com/example/demo/controller/BlogController.java
+++ src/main/java/com/example/demo/controller/BlogController.java
@@ -10,13 +10,17 @@ import com.example.demo.model.service.BlogService;
 import com.example.demo.model.service.TestService; // 최상단 서비스 클래스 연동 추가
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
  import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
  @Controller // 컨트롤러 어노테이션 명시
 public class BlogController {
@@ -31,13 +35,41 @@ public class BlogController {
 //model.addAttribute("articles", list); // 모델에 추가
 //return "article_list"; // .HTML 연결
 //}
- @GetMapping("/board_list") // 새로운 게시판 링크 지정
-public String board_list(Model model) {
- List<Board> list = blogService.findAll(); // 게시판 전체 리스트, 기존 Article에서 Board로 변경됨
+ //@GetMapping("/board_list") // 새로운 게시판 링크 지정
+//public String board_list(Model model) {
+ //List<Board> list = blogService.findAll(); // 게시판 전체 리스트, 기존 Article에서 Board로 변경됨
+//model.addAttribute("boards", list); // 모델에 추가
+//return "board_list"; // .HTML 연결
+//}
+
+@GetMapping("/board_list") // 새로운 게시판 링크 지정
+public String board_list(Model model, @RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "") String keyword) {
+PageRequest pageable = PageRequest.of(page, 3); // 한 페이지의 게시글 수
+Page<Board> list; // Page를 반환
+if (keyword.isEmpty()) {
+list = blogService.findAll(pageable); // 기본 전체 출력(키워드 x)
+} else {
+list = blogService.searchByKeyword(keyword, pageable); // 키워드로 검색
+}
 model.addAttribute("boards", list); // 모델에 추가
+model.addAttribute("totalPages", list.getTotalPages()); // 페이지 크기
+model.addAttribute("currentPage", page); // 페이지 번호
+model.addAttribute("keyword", keyword); // 키워드
 return "board_list"; // .HTML 연결
 }
 
+
+// 글쓰기 게시판
+@GetMapping("/board_write")
+public String board_write() {
+return "board_write";
+}
+
+@PostMapping("/api/boards") // 글쓰기 게시판 저장
+public String addboards(@ModelAttribute AddArticleRequest request) {
+blogService.save(request);
+return "redirect:/board_list"; // .HTML 연결
+}
 
   @GetMapping("/board_view/{id}") // 게시판 링크 지정
 public String board_view(Model model, @PathVariable Long id) {
--- src/main/java/com/example/demo/model/service/BlogService.java
+++ src/main/java/com/example/demo/model/service/BlogService.java
@@ -3,6 +3,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import com.example.demo.model.domain.Article;
 import com.example.demo.model.domain.Board;
@@ -26,15 +28,28 @@ public List<Board> findAll() { // 게시판 전체 목록 조회
 return blogRepository2.findAll();
 }
 
-public Article save(AddArticleRequest request){
+//public Article save(AddArticleRequest request){
 // DTO가 없는 경우 이곳에 직접 구현 가능
 // public ResponseEntity<Article> addArticle(@RequestParam String title, @RequestParam String content) {
 // Article article = Article.builder()
 // .title(title)
 // .content(content)
 // .build();
-return blogRepository.save(request.toEntity());
+//return blogRepository.save(request.toEntity());
+//}
+
+public Board save(AddArticleRequest request){
+// DTO가 없는 경우 이곳에 직접 구현 가능
+return blogRepository2.save(request.toEntity());
 }
+
+public Page<Board> findAll(Pageable pageable) {
+return blogRepository2.findAll(pageable);
+}
+
+public Page<Board> searchByKeyword(String keyword, Pageable pageable) {
+return blogRepository2.findByTitleContainingIgnoreCase(keyword, pageable);
+} // LIKE 검색 제공(대소문자 무시)
 
 //public Optional<Article> findById(Long id) { // 게시판 특정 글 조회
 //return blogRepository.findById(id);
